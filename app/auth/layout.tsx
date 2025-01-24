59d05498347466a112115ec29a5a4aa1fe156f5e
mayank-vashishtt <mayankvashishtt@gmail.com>
2025-01-24 05:54:21 +0530

    layouts in depth

---
+++ app/auth/layout.tsx
@@ -0,0 +1,22 @@
+export default  function({children} : {
+    children : React.ReactNode
+
+}
+){
+
+    return <div>
+    
+    <div className= "border-b text-center">
+        off hai bhyii 20 percent 
+    </div>
+
+
+    {children}
+
+
+    </div>
+
+    
+
+
+}
